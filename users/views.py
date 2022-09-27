68e9e7a888e3560487d0b968ba25125fb7a836e1
cdosorio <cdosorio@gmail.com>
2022-09-27 20:38:45 -0300

    Added messages form

--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Skill, Message
-from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .forms import CustomUserCreationForm, MessageForm, ProfileForm, SkillForm
 from .utils import paginateProfiles, searchProfiles
 
 def loginUser(request):    
@@ -173,3 +173,31 @@ def viewMessage(request, pk):
         message.save()
     context ={'message':message}
     return render(request, 'users/message.html', context)
+
+
+def createMessage(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+    
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+        
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+            
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+            
+            messages.success(request, 'Your message was successfully sent')
+            return redirect('user-profile', pk=recipient.id)        
+    
+    context ={'recipient':recipient , 'form':form}
+    return render(request, 'users/message_form.html', context)
