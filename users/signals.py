59225247ca837c5d94fecda9325d3a2367cceea3
cdosorio <cdosorio@gmail.com>
2022-09-28 07:50:56 -0300

    Send welcome emails

--- users/signals.py
+++ users/signals.py
@@ -2,35 +2,49 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import Profile
+
 
 # Listeners
 def createProfile(sender, instance, created, **kwargs):
-   if created:
-       user = instance
-       profile = Profile.objects.create(
-           user = user,
-           username = user.username,
-           email = user.email,
-           name = user.first_name
-       ) 
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name
+        )
         
-    
+        subject = 'Welcome to DevSearch'
+        body = 'We are glad you are here'
+
+        send_mail(
+            subject,
+            body,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False
+        )
+
+
 def updateUser(sender, instance, created, **kwargs):
     profile = instance
-    user = profile.user # 1 to 1 relation
-    if created == False: # to avoid infinite loop, check that profile not just created
+    user = profile.user  # 1 to 1 relation
+    if created == False:  # to avoid infinite loop, check that profile not just created
         user.first_name = profile.name
         user.username = profile.username
         user.email = profile.email
         user.save()
-                
-    
+
+
 def deleteUser(sender, instance, **kwargs):
     print('Profile deleted. Deleting user...')
     user = instance.user
     user.delete()
-        
+
 
 post_save.connect(createProfile, sender=User)
 post_save.connect(updateUser, sender=Profile)
