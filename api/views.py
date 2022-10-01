35295a54ddb9fb5d77abc07fcf0d9a67dd399164
cdosorio <cdosorio@gmail.com>
2022-09-30 21:07:35 -0300

    Adding JWT

--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
 from projects.models import Project
 
 
 @api_view(['GET'])
+#@permission_classes([IsAuthenticated])
 def getRoutes(request):
     routes = [
         {'GET':'/api/projects'},
@@ -18,6 +20,7 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+#@permission_classes([IsAuthenticated])
 def getProjects(request):
     projects = Project.objects.all()
     # convert to JSON data
